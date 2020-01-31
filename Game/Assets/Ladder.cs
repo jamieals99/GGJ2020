8d5beeb4b8b1a15d01954ac28785656601cd5ae9
1902474 <1902474@uad.ac.uk>
2020-01-31 23:54:54 +0000

    Added player movement and ladder stuff

---
+++ Game/Assets/Ladder.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.attachedRigidbody.isKinematic = true;
+            other.attachedRigidbody.gravityScale = 0.0f;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.attachedRigidbody.isKinematic = true;
+            other.attachedRigidbody.gravityScale = 0.0f;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.attachedRigidbody.isKinematic = false;
+            other.attachedRigidbody.gravityScale = 1;
+        }
+    }
+}
---
+++ Game/Assets/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speed;
+    bool inLadder;
+    bool inBottomPlatform;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5f;
+        gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) && Time.deltaTime > 0)
+        {
+            
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A) && Time.deltaTime > 0)
+        {
+
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W) && inLadder && Time.deltaTime > 0)
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S) && inLadder && !inBottomPlatform && Time.deltaTime > 0)
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+      if (other.gameObject.tag == "Ladder")
+        {
+            inLadder = true;
+        }
+
+      if(other.gameObject.tag == "Bottom Platform")
+        {
+            inBottomPlatform = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ladder")
+        {
+            inLadder = false;
+        }
+        if (other.gameObject.tag == "Bottom Platform")
+        {
+            inBottomPlatform = false;
+        }
+    }
+}
